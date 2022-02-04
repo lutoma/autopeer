9844074f1936d230c65f9bc6dc52af779f07f5b6
Lukas Martini <hello@lutoma.org>
2022-02-04 22:27:41 +0100

    Load bootstrap css/js and fonts locally for privacy

--- autopeer/settings.py
+++ autopeer/settings.py
@@ -96,10 +96,14 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static-collect/")
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
 LOGIN_REDIRECT_URL = '/peerings/'
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp         = require('gulp'),
+	sass         = require('gulp-sass')(require('sass'));
+	autoprefixer = require('gulp-autoprefixer')
+	strip        = require('gulp-strip-css-comments')
+	del          = require('del')
+
+// Compile SCSS files to CSS
+gulp.task('scss', () => {
+	del(['static/css/screen.css'])
+	return gulp.src(['scss/screen.scss'])
+	.pipe(sass({
+		outputStyle : 'compressed',
+		includePaths: 'node_modules'
+	}))
+	.pipe(autoprefixer())
+	.pipe(strip({preserve: false}))
+	.pipe(gulp.dest('static/css'))
+})
+
+gulp.task('fonts', () => {
+	return gulp.src([
+		'node_modules/@fortawesome/fontawesome-pro/webfonts/*',
+		'node_modules/typeface-fira-sans/files/*'
+	])
+	.pipe(gulp.dest('static/fonts'))
+})
+
+gulp.task('js-libs', () => {
+	return gulp.src([
+		'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+	])
+	.pipe(gulp.dest('static/js'))
+})
+
+// Watch asset folder for changes
+gulp.task('watch', gulp.series('scss', 'js-libs', 'fonts', () => {
+	gulp.watch('scss/**/*', gulp.series('scss'))
+}))
+
+gulp.task('build', gulp.series('scss', 'js-libs', 'fonts'))
+gulp.task('default', gulp.series('watch'))
